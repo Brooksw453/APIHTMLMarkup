58b5a604c4ed267fd62dbcee3ed4755223762a4a
Brooks Winchell <122840510+Brooksw453@users.noreply.github.com>
2023-12-09 19:53:05 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,22 @@
+// Existing frontend code
+
 const submitBtn = document.getElementById('submit');
 
-submitBtn.addEventListener('click', () => {
-  // Call API
+submitBtn.addEventListener('click', async () => {
+
+  // Get prompt from text box
+  const prompt = document.getElementById('prompt').value;
+
+  // Call serverless function 
+  const response = await fetch('https://<function-url>', {
+    method: 'POST',
+    body: JSON.stringify({prompt})  
+  });
+  
+  // Get completions
+  const {completions} = await response.json();
+
   // Display completions
+  document.getElementById('completions').textContent = completions;
+
 });
